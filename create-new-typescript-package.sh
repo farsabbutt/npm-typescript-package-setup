5b252f74e3de9e438f91d9ded67058bd8d65ee84
Farsab Butt <farsabb@gmail.com>
2022-06-04 13:47:25 +0200

    Added additional steps required to build the npm package

--- create-new-typescript-package.sh
+++ create-new-typescript-package.sh
@@ -51,8 +51,8 @@ EOT
 echo "Creating source folder and main package file"
 mkdir src && echo "export const Greeter = (name: string) => 'Hello there'; " >> src/index.ts
 
-echo "Configuring build script in package.json"
-npm install -g npm-add-script && \
+echo "Configuring build script in package.json" && \
+#npm install -g npm-add-script && \
 npx npm-add-script -k "build" -v "tsc" && \
 
 echo "Building the package" && \
@@ -79,7 +79,7 @@ cat <<EOT >> .prettierrc
 EOT
 
 echo "Configuring format and lint scripts in package.json" && \
-npm install -g npm-add-script && \
+#npm install -g npm-add-script && \
 npx npm-add-script -k "format" -v "prettier --write \"src/**/*.ts\"" && \
 npx npm-add-script -k "lint" -v "tslint -p tsconfig.json" && \
 
@@ -87,4 +87,35 @@ echo "Linting" && \
 npm run lint && \
 
 echo "Formatting" && \
-npm run format
+npm run format && \
+
+echo "Adding configuration to whitelist build files that need to be pushed to npm registry" && \
+node -e "let pkg=require('./package.json'); pkg.files=[\"lib/**/*\"]; require('fs').writeFileSync('package.json', JSON.stringify(pkg, null, 2));" && \
+
+echo "Setup jest for testing" && \
+npm install --save-dev jest ts-jest @types/jest &&
+
+echo "Add jest configuration" && \
+cat <<EOT >> jestconfig.json
+{
+  "transform": {
+    "^.+\\\\.(t|j)sx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\\\.|/)(test|spec))\\\\.(jsx?|tsx?)$",
+  "moduleFileExtensions": ["ts", "tsx", "js", "jsx", "json", "node"]
+}
+EOT
+node -e "let pkg=require('./package.json'); pkg.scripts.test='jest --config jestconfig.json'; require('fs').writeFileSync('package.json', JSON.stringify(pkg, null, 2));" && \
+mkdir src/__tests__ && \
+cat <<EOT >> src/__tests__/index.test.ts
+import { Greeter } from '../index';
+test('Main Test', () => {
+  expect(Greeter('')).toBe('Hello there');
+});
+EOT
+
+echo "Running tests" && \
+npm test && \
+
+echo "Configuring npm scripts: 'prepare', 'prepublishOnly', 'preversion', 'version', 'postversion'" && \
+node -e "let pkg=require('./package.json'); pkg.scripts.prepare='npm run build'; pkg.scripts.prepublishOnly='npm test && npm run lint'; pkg.scripts.preversion='npm run lint'; pkg.scripts.version='npm run format && git add -A src'; pkg.scripts.postversion='git push && git push --tags'; require('fs').writeFileSync('package.json', JSON.stringify(pkg, null, 2));"
